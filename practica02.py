9c103ffc248891b530a749ac58b06b3e4e301ac2
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-02-06 17:40:15 -0600

    Clase reloj

--- practica02.py
+++ practica02.py
@@ -1,25 +1,30 @@
 #NOMBRE PROGRAMADOR: Alejandro Cruz Cruz
-#DESCRIPCION: Crear clase para el uso de un banco
-#FECHA: 01/02/2024
-#HORA: 19:24
+#DESCRIPCION: Crear clase Reloj el cual dara la hora en el formato adecuado
+#FECHA: 06/02/2024
+#HORA: 17:38
 
-class Cuenta:
-    def __init__(self,nombre,saldo) -> None:
-        self._nombre=nombre
-        if (saldo > 0.0):
-            self._saldo=saldo
-    
-    def depositar(self,montoDeposito) -> None:
-        if(montoDeposito > 0.0):
-            self._saldo+=montoDeposito
+class Tiempo1:
+    def __init__(self, hora, minuto,segundo):
+        self.hora = hora
+        self.minuto = minuto
+        self.segundo = segundo
 
-    def obtenerSaldo(self) -> float:
-        return self._saldo
+    def establecerTiempo(self, hora, minuto, segundo ):
+        if (hora<0 or hora >=24 or minuto <0 or minuto >=60 or segundo <0 or segundo >=60):
+            print("Error Hora invalida")
+        else:
+            self.hora = hora
+            self.minuto = minuto
+            self.segundo = segundo
     
-    def establecerNombre(self,nombre):
-        self._nombre=nombre
+    def aStringUniversal(self):
+        return str(f"{self.hora:02.0f}:{self.minuto:02.0f}:{self.segundo:02.0f}")
+    
+    def toString(self):
+        texto = 12 if (self.hora==0 or self.hora==12) else self.hora%12
+        meridiano = "AM" if self.hora <12 else "PM"
+        return str(f" {texto:02.0f}:{self.minuto:02.0f}:{self.segundo:02.0f} {meridiano}")
 
-    def obtenerNombre(self) -> str:
-        return self._nombre
-    
-miCuenta=Cuenta('Alejandro',10)
+hora1 = Tiempo1(23,8,8)
+print(hora1.toString())
+print(hora1.aStringUniversal())
