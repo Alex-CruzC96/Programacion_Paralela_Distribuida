389ede31fd09329c54c3189211dd7e4205a296b6
Alejandro Cruz <alexcruz_c96@outlook.com>
2024-02-20 11:58:08 -0600

    Submenu 2

--- HTML/script.js
+++ HTML/script.js
@@ -1,6 +1,6 @@
 //Comportamiento de la web
 
-//menu de categorias principal
+//menu de categorias principal opcion 1
 let varOne=document.getElementById("opcion1");
 let hidenOne=document.getElementById("hiden-box-one");
 
@@ -9,4 +9,13 @@ varOne.addEventListener('mouseover',function(){
 });
 varOne.addEventListener('mouseleave',function(){
     hidenOne.classList.remove("active");
+});
+//submenu numero 2
+let varTwo=document.getElementById("opcion2");
+let hidenTwo=document.getElementById("hiden-box-two");
+varTwo.addEventListener('mouseover',function(){
+    hidenTwo.classList.add("active")
+});
+varTwo.addEventListener('mouseleave',function(){
+    hidenTwo.classList.remove("active");
 });
