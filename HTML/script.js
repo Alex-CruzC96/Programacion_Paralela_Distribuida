5ed21b1967dba780c7643d932fcbc50135b3bc35
heberjfet <heber.alvaro49@unach.mx>
2024-02-23 01:25:16 -0600

    reel de fotos- falta mejorar

--- HTML/script.js
+++ HTML/script.js
@@ -1,38 +1,47 @@
 //Comportamiento de la web
 
 //menu de categorias principal opcion 1
-let varOne=document.getElementById("opcion1");
-let hidenOne=document.getElementById("hiden-box-one");
+let varOne = document.getElementById("opcion1");
+let hidenOne = document.getElementById("hiden-box-one");
 
-varOne.addEventListener('mouseover',function(){
+varOne.addEventListener('mouseover', function () {
     hidenOne.classList.add("active");
 });
-varOne.addEventListener('mouseleave',function(){
+varOne.addEventListener('mouseleave', function () {
     hidenOne.classList.remove("active");
 });
 //submenu numero 2
-let varTwo=document.getElementById("opcion2");
-let hidenTwo=document.getElementById("hiden-box-two");
-varTwo.addEventListener('mouseover',function(){
+let varTwo = document.getElementById("opcion2");
+let hidenTwo = document.getElementById("hiden-box-two");
+varTwo.addEventListener('mouseover', function () {
     hidenTwo.classList.add("active")
 });
-varTwo.addEventListener('mouseleave',function(){
+varTwo.addEventListener('mouseleave', function () {
     hidenTwo.classList.remove("active");
 });
 //submenu numero 3
-let varThree=document.getElementById("opcion3");
-varThree.addEventListener('mouseover',function(){
+let varThree = document.getElementById("opcion3");
+varThree.addEventListener('mouseover', function () {
     document.getElementById("hiden-box-three").classList.add("active")
 });
-varThree.addEventListener('mouseleave',function(){
+varThree.addEventListener('mouseleave', function () {
     document.getElementById("hiden-box-three").classList.remove("active")
 });
 
 //submenu numero 4
-let varFour=document.getElementById("opcion4");
-varFour.addEventListener('mouseover', function(){
+let varFour = document.getElementById("opcion4");
+varFour.addEventListener('mouseover', function () {
     document.getElementById("hiden-box-four").classList.add("active")
 });
-varFour.addEventListener('mouseleave', function(){
+varFour.addEventListener('mouseleave', function () {
     document.getElementById("hiden-box-four").classList.remove("active")
 });
+//reel de imagen
+let indice = 0;
+function mover(direccion) {
+    const fotos = document.querySelectorAll('.foto');
+    const totalFotos = fotos.length;
+    indice = (indice + direccion + totalFotos) % totalFotos;
+    const carrusel = document.querySelector('.carrusel');
+    carrusel.scrollLeft = fotos[indice].offsetLeft;
+}
